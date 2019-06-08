ef5036e3790f8bb187b77780ad870d04e2814d12
João Vitor <jvsms13@gmail.com>
2019-06-08 13:56:51 -0300

    API Rest adicionada no framework Flask. Retorna datetime por enquanto.

--- AppServer.py
+++ AppServer.py
@@ -1,29 +1,25 @@
-from flask import Flask
-from flask import jsonify
-from flask import request
+from flask import Flask, request
+from flask_restful import Resource, Api
+from datetime import datetime #builtin
+import json #builtin
 
 app = Flask(__name__)
+api = Api(app)
 
-info_list = \
-    [
-        {'Horário da requisição': 'xpto',
-    'Nome do container': 'xpto',
-    'ID do container': 'xpto',
-    'ID da imagem': 'xpto',
-    'Uso de CPU': 'xpto',
-    'Uso de Memória': 'xpto', }
-    ]
+class SysInfo(Resource):
+	INFO_LIST = []
 
-@app.route('/', methods=['GET'])
-def get_info():
-    return jsonify({'info': info_list})
+	def get(self):
+		print("GET solicitado em {}".format(datetime.now().time()))
+		return {'info': SysInfo.INFO_LIST}
 
-@app.route('/info', methods=['POST'])
-def post_info():
-    info = {'info': request.json['info']}
-    info_list.append(info)
-    print(info_list)
-    return jsonify({'info': info_list})
+	def post(self):
+		info = request.get_json()
+		print("POST recebido em {}\n".format(datetime.now().time(), json.dumps(info, indent=2)))
+		SysInfo.INFO_LIST.append(info)
+		return info
+
+api.add_resource(SysInfo, '/')
 
 if __name__ == '__main__':
-    app.run(host='localhost', port='5000')
+    app.run(host='localhost', port='5000', debug=True)
