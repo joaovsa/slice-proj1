1d8b578eaba793cc217b3ed9cdc07e24f8c038cf
João Vitor <jvsms13@gmail.com>
2019-06-08 13:57:34 -0300

    Client evia e recebe datetime via get e post

--- AppClient.py
+++ AppClient.py
@@ -1,10 +1,12 @@
 import requests
+from datetime import datetime
+
+# Método POST
+data = {'info': datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}
+r = requests.post('http://localhost:5000/', json=data)
+print("POST ENVIADO OK > {}".format(r.json()))
 
 # Método GET
 r = requests.get('http://localhost:5000/')
-print(r.json())
+print("GET OK > {}".format(r.json()))
 
-# Método POST
-data = {'info': 'teste'}
-r = requests.post('http://localhost:5000/info', json=data)
-print(r.json())
