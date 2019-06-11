069cf8df815597218f2630b72a3ef6bbf6b1fc20
João Vitor <jvsms13@gmail.com>
2019-06-10 22:59:46 -0300

    ip corrigido

--- app/client/AppClient.py
+++ app/client/AppClient.py
@@ -3,10 +3,10 @@ from datetime import datetime
 
 # Método POST
 data = {'info': datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}
-r = requests.post('http://198.162.50.2:2377/', json=data)
+r = requests.post('http://192.168.50.2:2377/', json=data)
 print("POST ENVIADO OK > {}".format(r.json()))
 
 # Método GET
-r = requests.get('http://198.162.50.2:2377/')
+r = requests.get('http://192.168.50.2:2377/')
 print("GET OK > {}".format(r.json()))
 
