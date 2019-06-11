19e799eea3dd8e2f6aa4800ab1515cc4155b56f8
João Vitor <jvsms13@gmail.com>
2019-06-10 22:23:59 -0300

    portas de acesso alteradas nos codigos py

--- app/client/AppClient.py
+++ app/client/AppClient.py
@@ -3,10 +3,10 @@ from datetime import datetime
 
 # Método POST
 data = {'info': datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}
-r = requests.post('http://localhost:5000/', json=data)
+r = requests.post('http://198.162.50.2:5000/', json=data)
 print("POST ENVIADO OK > {}".format(r.json()))
 
 # Método GET
-r = requests.get('http://localhost:5000/')
+r = requests.get('http://198.162.50.2:5000/')
 print("GET OK > {}".format(r.json()))
 
